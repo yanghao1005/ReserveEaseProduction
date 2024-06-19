d10591008b418fc188237df6230e97bba1f50795
yanghao1005 <yanghao1005@gmail.com>
2024-06-19 12:18:01 +0200

    front api url changes

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiUrl = "/choreo-apis/reserveeaseproduction/backend/v1";
 // Crea una instancia de Axios
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 // Configura un interceptor para adjuntar el token de acceso a cada solicitud
